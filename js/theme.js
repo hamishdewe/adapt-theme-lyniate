08d82b621fe3ca7aea333601f39907c02d6e8236
Hamish Dewe <hamish.dewe@gmail.com>
2021-02-16 13:30:34 +1300

    Adds commenting

--- js/theme.js
+++ js/theme.js
@@ -7,16 +7,47 @@ define([
 ], function(Adapt, ThemePageView, ThemeArticleView, ThemeBlockView, ThemeView) {
   
   function onPageView() {
+    // Do PrismJS formatting for all non-excepted components
     var components = $('div.component:not(".no-prism"):not(".prism-preformatted")')
     $(components).each((idx, component) => {
       Prism.highlightAllUnder(component)
     });
     
+    // Remove extraneous spaces from formatted code blocks
     var blocks = $('code[class*=language-]');
     for (var i = 0; i < blocks.length; i++) {
       blocks[i].innerHTML = blocks[i].innerHTML.replace(/\n\s{2,}?/g, '\n');
     }
     
+    // Add comment links
+    window.Adapt = window.Adapt || Adapt;
+    if (!Adapt.course.attributes._lyniate._comments || !Adapt.course.attributes._lyniate._mailto || !Adapt.course.attributes._lyniate._editor) {
+      return;
+    }
+    $($('div.page')).each((idx, item) => {
+      addCommentLink(item, 'page');
+    });
+    $($('div.article')).each((idx, item) => {
+      addCommentLink(item, 'article');
+    });
+    $($('div.block')).each((idx, item) => {
+      addCommentLink(item, 'block');
+    });
+    $($('div.component')).each((idx, item) => {
+      addCommentLink(item, 'component');
+    });
+  }
+  
+  function addCommentLink(el, type) {
+    var courseId = Adapt.config.attributes._courseId;
+    var itemId = el.attributes['data-adapt-id'].value;
+    var titleEl = $(el).find(`.${type}__title .${type}__title-inner`)[0];
+    if (!titleEl) {
+      return;
+    }
+    var subject = encodeURI(`Comment on ${type} '${titleEl.innerText}'`);
+    var body = encodeURI(`\r\n\r\nLink: ${Adapt.course.attributes._lyniate._editor}/#editor/${courseId}/${type}/${itemId}/edit`);
+    $(`<a style="display:none" class="comment-link" title="Comment on ${type} '${titleEl.innerText}'" href="mailto:${Adapt.course.attributes._lyniate._mailto}?subject=${subject}&body=${body}"></a>`).insertAfter(titleEl);
   }
 
   function onDataReady() {
